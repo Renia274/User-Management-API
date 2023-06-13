48ea6496b6fcf428c0107db1930fe5a810c9c731
Renia Dioleti <reniadiol3@gmail.com>
2023-06-13 18:19:41 +0300

    Update UserService.java

--- src/main/java/com/example/test/UserService.java
+++ src/main/java/com/example/test/UserService.java
@@ -2,8 +2,10 @@ package com.example.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+Certainly! Here's the code snippet with comments added to explain the different parts of the code:
+
+java
 
 @Service
 public class UserService {
@@ -16,28 +18,32 @@ public class UserService {
     }
 
     public void saveUser(User user) {
+        // Save the user to the database
         userRepository.save(user);
     }
 
     public User registerUser(User user) {
+        // Register a new user by saving them to the database
         return userRepository.save(user);
     }
 
     public User getUserById(int id) {
+        // Retrieve a user from the database by their ID
         return userRepository.findById(id).orElse(null);
     }
 
     public List<User> getAllUsers() {
+        // Retrieve all users from the database
         return userRepository.findAll();
     }
 
     public void updateUser(User user) {
+        // Update an existing user by saving the changes to the database
         userRepository.save(user);
     }
 
     public void deleteUser(int id) {
+        // Delete a user from the database by their ID
         userRepository.deleteById(id);
     }
-
-
-}
+ }
