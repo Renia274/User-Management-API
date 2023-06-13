40270bb94cca4f20c077120a5e285e7a916e570b
Renia Dioleti <reniadiol3@gmail.com>
2023-06-13 18:16:43 +0300

    Update UserRepository.java

--- src/main/java/com/example/test/UserRepository.java
+++ src/main/java/com/example/test/UserRepository.java
@@ -2,13 +2,15 @@ package com.example.test;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
-
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+    // Find a user by their name
     User findByName(String name);
+    
+    // Find a user by their email
     User findByEmail(String email);
+    
+    // Find a user by their password
     User findByPassword(String password);
 }
 
