d699f71dc774127be3cdfedfb83ddb5299da196c
Renia Dioleti <reniadiol3@gmail.com>
2023-06-13 18:15:39 +0300

    Update UserController.java

--- src/main/java/com/example/test/UserController.java
+++ src/main/java/com/example/test/UserController.java
@@ -2,7 +2,6 @@ package com.example.test;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
@@ -18,29 +17,33 @@ public class UserController {
 
     @PostMapping
     public void createUser(@RequestBody User user) {
+        // Endpoint for creating a new user
         userService.saveUser(user);
     }
 
     @GetMapping("/{id}")
     public User getUserById(@PathVariable int id) {
+        // Endpoint for retrieving a user by their ID
         return userService.getUserById(id);
     }
 
     @GetMapping
     public List<User> getAllUsers() {
+        // Endpoint for retrieving all users
         return userService.getAllUsers();
     }
 
     @PutMapping("/{id}")
     public void updateUser(@PathVariable int id, @RequestBody User user) {
+        // Endpoint for updating a user with a given ID
         user.setId(id);
         userService.updateUser(user);
     }
 
     @DeleteMapping("/{id}")
     public void deleteUser(@PathVariable int id) {
+        // Endpoint for deleting a user with a given ID
         userService.deleteUser(id);
     }
-
-
 }
+
