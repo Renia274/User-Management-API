3c0bf385ff0d308a3f5470b3b33db07047a92dc2
Renia Dioleti <reniadiol3@gmail.com>
2023-06-21 19:02:29 +0300

    Update UserService.java

--- src/main/java/com/example/test/UserService.java
+++ src/main/java/com/example/test/UserService.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+
 @Service
 public class UserService {
 
@@ -15,18 +16,20 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    // Save a user to the database
     public void saveUser(User user) {
         userRepository.save(user);
     }
 
+    // Get a user by their ID
     public User getUserById(int id) {
         return userRepository.findById(id).orElse(null);
     }
 
+    // Get all users based on a filtering condition and pagination
     public Page<User> getAllUsers(String filter, Pageable pageable) {
         Specification<User> specification = (root, query, criteriaBuilder) -> {
             // Add your filtering conditions based on the 'filter' parameter
-            // Example: filtering by name
             if (!filter.isEmpty()) {
                 return criteriaBuilder.like(root.get("name"), "%" + filter + "%");
             }
@@ -36,13 +39,14 @@ public class UserService {
         return userRepository.findAll(specification, pageable);
     }
 
+    // Update a user in the database
     public void updateUser(User user) {
         userRepository.save(user);
     }
 
+    // Delete a user from the database based on their ID
     public void deleteUser(int id) {
         userRepository.deleteById(id);
     }
-
 
 }
