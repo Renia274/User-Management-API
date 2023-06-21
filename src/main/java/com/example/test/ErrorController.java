aad9e8ee64effe1b95b84cf912f48cbea7b8c0af
Renia Dioleti <reniadiol3@gmail.com>
2023-06-21 19:03:14 +0300

    Update ErrorController.java

--- src/main/java/com/example/test/ErrorController.java
+++ src/main/java/com/example/test/ErrorController.java
@@ -11,17 +11,23 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
 
     @RequestMapping("/error")
     public ResponseEntity<ErrorResponse> handleError(HttpServletRequest request) {
+        // Get the status code and error message from the request attributes
         Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
         String message = (String) request.getAttribute("javax.servlet.error.message");
+
+        // Create an ErrorResponse object with the status code and message
         ErrorResponse errorResponse = new ErrorResponse(statusCode, message);
+
+        // Return a ResponseEntity containing the ErrorResponse and corresponding HttpStatus
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(statusCode));
     }
 
-
+    // Get the path for handling errors
     public String getErrorPath() {
         return "/error";
     }
 
+    // ErrorResponse class to hold the status code and message
     public static class ErrorResponse {
         private int status;
         private String message;
@@ -48,4 +54,3 @@ public class ErrorController implements org.springframework.boot.web.servlet.err
         }
     }
 }
-
