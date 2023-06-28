b5203233d2fe96547089253748775a7310621a29
Dioleti Renia <reniadiol3@gmail.com>
2023-06-28 17:21:47 +0300

    Allow access to all endpoints

--- src/main/java/com/example/test/SecurityConfig.java
+++ src/main/java/com/example/test/SecurityConfig.java
@@ -19,10 +19,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/users/insert").permitAll() // Allow access to the user creation endpoint without authentication
+                .antMatchers("/", "/api/**").permitAll() // Allow access to the root ("/") and "/api/**" paths without authentication
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic().disable()
                 .formLogin().disable();
     }
+
 }
