592141f2826c552283e8f0499d96b11ef83262d4
Renia Dioleti <reniadiol3@gmail.com>
2024-07-15 17:53:45 +0300

    Implement access to service with admin

--- src/main/java/com/example/test/SecurityConfig.java
+++ src/main/java/com/example/test/SecurityConfig.java
@@ -2,13 +2,15 @@ package com.example.test;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
@@ -16,14 +18,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/", "/api/**", "/login","/forgot-password").permitAll()
                 .anyRequest().authenticated()
-                .and()
-                .httpBasic().disable()
-                .formLogin().disable();
+                  .and()
+                .formLogin().permitAll();
+
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("testuser")
+                .password(passwordEncoder().encode("testpassword"))
+                .roles("USER");
     }
 }
