445f8596d1c29442c12cbb174c096a981b359062
Renia Dioleti <reniadiol3@gmail.com>
2024-07-15 16:25:46 +0300

    Implement forgot password

--- src/main/java/com/example/test/SecurityConfig.java
+++ src/main/java/com/example/test/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.example.test;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,12 +20,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/", "/api/**", "/login").permitAll() // Allow access to the root ("/"), "/api/**", and "/login" paths without authentication
+                .antMatchers("/", "/api/**", "/login","/forgot-password").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic().disable()
                 .formLogin().disable();
     }
-
-
 }
